dc87c29cdbbab5b3f4dc36e5a34e626b286f9d84
Stanislav Pirx <stanislav.pirx@gmail.com>
2023-12-12 22:52:44 +0300

    nothing important
    Signed-off-by: Stanislav Pirx <stanislav.pirx@gmail.com>

--- nvim/lua/pronvis/init.lua
+++ nvim/lua/pronvis/init.lua
@@ -3,15 +3,9 @@ require("pronvis.packer")
 require("pronvis.remap")
 
 -- Colors
--- Base16
-vim.cmd('let base16colorspace=256')
 -- personal voting:
 -- 1st: base16-tomorrow-night-eighties
 -- 2nd: base16-gruvbox-dark-soft
 -- 3rd: base16-monokai
 -- 4th: base16-woodland
---if filereadable(expand("~/.vimrc_background"))
---   source ~/.vimrc_background
---else
-   vim.cmd('colorscheme base16-tomorrow-night-eighties')
---endif
+vim.cmd('colorscheme base16-tomorrow-night-eighties')
