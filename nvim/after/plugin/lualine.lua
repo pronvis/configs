76fd011921e28e08f70704af832c5d020cb5fea9
Stanislav Pirx <stanislav.pirx@gmail.com>
2023-12-14 23:09:55 +0300

    edit lualine config a bit
    Signed-off-by: Stanislav Pirx <stanislav.pirx@gmail.com>

--- nvim/after/plugin/lualine.lua
+++ nvim/after/plugin/lualine.lua
@@ -3,26 +3,32 @@ function ObsStatus()
 end
 
 require('lualine').setup {
-  options = {
-    icons_enabled = true,
-    theme = 'seoul256',
-    component_separators = { left = '', right = ''},
-    section_separators = { left = '', right = ''},
-  },
-  sections = {
-    lualine_a = {'mode'},
-    lualine_b = {'branch', 'diff', 'diagnostics'},
-    lualine_c = {'filename'},
-    lualine_x = {ObsStatus, 'encoding', 'fileformat', 'filetype'},
-    lualine_y = {'progress' },
-    lualine_z = {'location'}
-  },
-  inactive_sections = {
-    lualine_a = {},
-    lualine_b = {},
-    lualine_c = {'filename'},
-    lualine_x = {'location'},
-    lualine_y = {},
-    lualine_z = {}
-  }
+    options = {
+        icons_enabled = true,
+        theme = 'seoul256',
+        component_separators = { left = '', right = '' },
+        section_separators = { left = '', right = '' },
+    },
+    sections = {
+        lualine_a = { 'mode' },
+        lualine_b = {
+            {
+                'filename',
+                path = 1,
+                shorting_target = 100
+            },
+            'diff', 'diagnostics' },
+        lualine_c = { 'branch' },
+        lualine_x = { ObsStatus, 'encoding', 'fileformat', 'filetype' },
+        lualine_y = { 'progress' },
+        lualine_z = { 'location' }
+    },
+    inactive_sections = {
+        lualine_a = {},
+        lualine_b = {},
+        lualine_c = { 'filename' },
+        lualine_x = { 'location' },
+        lualine_y = {},
+        lualine_z = {}
+    }
 }
