bb7c6eeef3ad1e7c5bcc5e231a0403aae94f3089
Stanislav Pirx <stanislav.pirx@gmail.com>
2023-12-20 23:39:41 +0300

    set focus on undotree toggle
    Signed-off-by: Stanislav Pirx <stanislav.pirx@gmail.com>

--- nvim/after/plugin/undotree.lua
+++ nvim/after/plugin/undotree.lua
@@ -1,1 +1,4 @@
 vim.keymap.set('n', '<leader>u', vim.cmd.UndotreeToggle)
+-- if set, let undotree window get focus after being opened, otherwise
+-- focus will stay in current window.
+vim.g.undotree_SetFocusWhenToggle = 1
