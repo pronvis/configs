13015f78e075ee0c7565f452a20ef81cb2a4a590
Stanislav Pirx <stanislav.pirx@gmail.com>
2023-12-27 17:37:28 +0300

    simplify lsp config
    Signed-off-by: Stanislav Pirx <stanislav.pirx@gmail.com>

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -31,39 +31,15 @@ end)
 require('mason').setup()
 require('mason-lspconfig').setup()
 
-local servers = {
-    html = { filetypes = { 'html'} },
-    rust_analyzer = {},
-
-    lua_ls = {
-        Lua = {
-            workspace = { checkThirdParty = false },
-            telemetry = { enable = false },
-            -- NOTE: toggle below to ignore Lua_LS's noisy `missing-fields` warnings
-            diagnostics = { disable = { 'missing-fields' } },
-        },
-    },
-}
-
 -- nvim-cmp supports additional completion capabilities, so broadcast that to servers
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
 
 -- Ensure the servers above are installed
+local servers = { 'html', 'lua_ls' }
 local mason_lspconfig = require('mason-lspconfig')
 mason_lspconfig.setup {
-  ensure_installed = vim.tbl_keys(servers),
-}
-
-mason_lspconfig.setup_handlers {
-  function(server_name)
-    require('lspconfig')[server_name].setup {
-      capabilities = capabilities,
-      on_attach = on_attach,
-      settings = servers[server_name],
-      filetypes = (servers[server_name] or {}).filetypes,
-    }
-  end,
+  ensure_installed = servers,
 }
 
 -- [[ Configure nvim-cmp ]]
