fe13f881330184c3faa6439f66910ec506eb1fa2
Stanislav Pirx <stanislav.pirx@gmail.com>
2023-12-30 00:20:37 +0300

    add cpp lang to treesitter
    Signed-off-by: Stanislav Pirx <stanislav.pirx@gmail.com>

--- nvim/after/plugin/treesitter.lua
+++ nvim/after/plugin/treesitter.lua
@@ -1,6 +1,6 @@
 require'nvim-treesitter.configs'.setup {
     -- A list of parser names, or "all" (the five listed parsers should always be installed)
-    ensure_installed = {"java", "javascript", "typescript", "bash", "c", "lua", "vim", "vimdoc", "query", "rust", "toml" },
+    ensure_installed = {"java", "javascript", "typescript", "bash", "c", "lua", "vim", "vimdoc", "query", "rust", "toml", "cpp" },
 
     -- Install parsers synchronously (only applied to `ensure_installed`)
     sync_install = false,
