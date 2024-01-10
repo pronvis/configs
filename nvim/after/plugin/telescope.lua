ad9fbf247d8ed361614031c8748308f671e39dc1
Stanislav Pirx <stanislav.pirx@gmail.com>
2024-01-10 16:08:30 +0300

    update telescope search config
    Signed-off-by: Stanislav Pirx <stanislav.pirx@gmail.com>

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -9,12 +9,28 @@ require('telescope').setup {
             '--column',
             '--smart-case',
             '--hidden',
-        }
-    }
+        },
+
+        file_ignore_patterns = {
+            "node_modules", "build", "dist", "yarn.lock", ".idea", ".git", ".vscode", "package-lock.json"
+        },
+    },
+
+    pickers = {
+        find_files = {
+            -- needed to exclude some files & dirs from general search
+            -- when not included or specified in .gitignore
+            find_command = {
+                "rg",
+                "--files",
+                "--hidden",
+            },
+        },
+    },
 }
 
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<C-p>', function() builtin.find_files({ hidden = true }) end, {})
+vim.keymap.set('n', '<C-p>', builtin.find_files, {})
 vim.keymap.set('n', '<leader>s', function()
     builtin.grep_string({ search = vim.fn.input("Grep > ") })
 end)
