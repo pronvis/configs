a385a598cc41dc54cc909489ef2663847e8ea26e
pronvis <stanislav.pirx@gmail.com>
2026-05-05 23:59:01 +0300

    update lsp config configuration, cause previous one is does not work anymore - outdated
    Signed-off-by: pronvis <stanislav.pirx@gmail.com>

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -38,19 +38,7 @@ vim.api.nvim_create_autocmd('LspAttach', {
 require('mason').setup()
 require('mason-lspconfig').setup()
 
-local servers = {
-    html = { filetypes = { 'html' } },
-    -- rust_analyzer = {}, -- 'rustaceanvim' have its own rust-analyzer config
-    clangd = {},
-    lua_ls = {
-        Lua = {
-            workspace = { checkThirdParty = false },
-            telemetry = { enable = false },
-            -- NOTE: toggle below to ignore Lua_LS's noisy `missing-fields` warnings
-            diagnostics = { disable = { 'missing-fields', 'undefined-global' } },
-        },
-    },
-}
+local servers = { html, clang, lua_ls, typescript, javascript }
 
 -- nvim-cmp supports additional completion capabilities, so broadcast that to servers
 local capabilities = vim.lsp.protocol.make_client_capabilities()
@@ -59,23 +47,36 @@ capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
 -- Ensure the servers above are installed
 local mason_lspconfig = require('mason-lspconfig')
 mason_lspconfig.setup {
-    ensure_installed = vim.tbl_keys(servers),
-}
-
-mason_lspconfig.setup {
-    function(server_name)
-        require('lspconfig')[server_name].setup {
-            capabilities = capabilities,
-            settings = servers[server_name],
-            filetypes = (servers[server_name] or {}).filetypes,
-        }
-    end,
+    ensure_installed = servers,
 }
 
 vim.lsp.config("clang", {
     cmd = {
         "clangd",
         "--offset-encoding=utf-16",
+    },
+})
+
+vim.lsp.config("lua_ls", {
+    settings = {
+        Lua = {
+            workspace = { checkThirdParty = false },
+            telemetry = { enable = false },
+            -- NOTE: toggle below to ignore Lua_LS's noisy `missing-fields` warnings
+            diagnostics = { disable = { 'missing-fields', 'undefined-global' } },
+        },
+    }
+})
+
+vim.lsp.config("ts_ls", {
+    capabilities = capabilities,
+    settings = {
+        typescript = {
+            format = { indentSize = 2, tabSize = 2, convertTabsToSpaces = true },
+        },
+        javascript = {
+            format = { indentSize = 2, tabSize = 2, convertTabsToSpaces = true },
+        },
     },
 })
 
