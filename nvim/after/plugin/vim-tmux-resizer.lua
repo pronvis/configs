78260e1e0b5200fda78d26b790c3c4dcbd60a424
pronvis <stanislav.pirx@gmail.com>
2026-07-05 17:38:39 +0300

    better nvim

--- nvim/after/plugin/vim-tmux-resizer.lua
+++ nvim/after/plugin/vim-tmux-resizer.lua
@@ -5,10 +5,18 @@ vim.g.tmux_resizer_resize_count = 5
 vim.g.tmux_resizer_vertical_resize_count = 5
 
 -- alt-h
-vim.api.nvim_set_keymap('n', '˙', ':TmuxResizeLeft<CR>', {noremap = true, silent = true})
+vim.api.nvim_set_keymap('n', '<A-h>', ':TmuxResizeLeft<CR>', { noremap = true, silent = true })
 -- alt-j
-vim.api.nvim_set_keymap('n', '∆', ':TmuxResizeDown<CR>', {noremap = true, silent = true})
+vim.api.nvim_set_keymap('n', '<A-j>', ':TmuxResizeDown<CR>', { noremap = true, silent = true })
 -- alt-k
-vim.api.nvim_set_keymap('n', '˚', ':TmuxResizeUp<CR>', {noremap = true, silent = true})
+vim.api.nvim_set_keymap('n', '<A-k>', ':TmuxResizeUp<CR>', { noremap = true, silent = true })
 -- alt-l
-vim.api.nvim_set_keymap('n', '¬', ':TmuxResizeRight<CR>', {noremap = true, silent = true})
+vim.api.nvim_set_keymap('n', '<A-l>', ':TmuxResizeRight<CR>', { noremap = true, silent = true })
+
+-- same resizers in terminal mode (e.g. the Claude Code terminal). alacritty.toml
+-- makes Option+h/j/k/l send ESC+<letter> (i.e. <A-h/j/k/l>), so map those here.
+-- <Cmd> runs the command without leaving terminal mode.
+vim.api.nvim_set_keymap('t', '<A-h>', '<Cmd>TmuxResizeLeft<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('t', '<A-j>', '<Cmd>TmuxResizeDown<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('t', '<A-k>', '<Cmd>TmuxResizeUp<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('t', '<A-l>', '<Cmd>TmuxResizeRight<CR>', { noremap = true, silent = true })
